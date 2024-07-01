7a66faa970e6fd8df60760c7dedbff551bd9623a
Amit <amitvbhagat1@gmail.com>
2024-07-01 20:04:43 +0530

    Add navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react' 
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-    Hello There this is amit bhagat
+    <Navbar />
     </>
   )
 }
