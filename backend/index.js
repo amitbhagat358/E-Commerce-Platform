e0fb850587e3b2a79fd8eb2bc3a442081a6a9163
Amit <amitvbhagat1@gmail.com>
2024-07-17 11:51:56 +0530

    Add User Routes

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,28 @@
-console.log("");
-console.log("hii there 👋");
+import express from 'express'
+import dotenv from 'dotenv'
+import userRoutes from './Routes/userRoutes.js'
+import connectToDB from './config/db.js';
+import cookieParser from 'cookie-parser';
 
+dotenv.config();
+connectToDB();
+
+
+const app = express();
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const port = process.env.PORT || 3000;
+
+console.log(process.env.PORT)
+
+
+app.use('/api/users', userRoutes);
+
+app.get('/', (req, res) =>{
+  res.send("Hello World !");
+})
+
+
+app.listen(port, ()=> console.log(`listning on port ${port}`));
