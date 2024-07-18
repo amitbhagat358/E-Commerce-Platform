3f4a62e58b000722b1cab58c353844d261095a2a
Amit <amitvbhagat1@gmail.com>
2024-07-18 12:41:21 +0530

    Add order routes

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import userRoutes from './Routes/userRoutes.js'
 import categoryRoutes from './Routes/categoryRoutes.js'
 import productRoutes from './Routes/productRoutes.js'
+import orderRoutes from './Routes/orderRoutes.js'
 import connectToDB from './config/db.js';
 import cookieParser from 'cookie-parser';
 
@@ -23,6 +24,7 @@ console.log(process.env.PORT)
 app.use('/api/users', userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products" , productRoutes);
+app.use("/api/orders", orderRoutes);
 
 
 app.get('/', (req, res) =>{
